1e89dacaf6653bea5b0b05771200f97500080222
Francisco Huerta <frhuerta.dwfsc16@ucamp.utel.edu.mx>
2023-03-26 21:24:15 -0600

    segundo cambio

--- js/localstorage.js
+++ js/localstorage.js
@@ -1,1 +1,16 @@
-console.log("gatitos");
+const nombre = document.getElementById("NombreInput");
+const apellido = document.getElementById("ApellidoInput");
+const correo = document.getElementById("correoelectronicoInput");
+const telefono = document.getElementById("NumeroTelefonicoinput");
+const usuarios = document.getElementById("usuarios") || [];
+
+const agregarUsuario = () => {
+  const usuario = {
+    id: crypto.randomUUID(),
+    nombre: nombre.value,
+    apellido: apellido.value,
+    correo: correo.value,
+    telefono: telefono.value,
+  };
+  console.log(usuario);
+};
