54fc4e5162d8eff42bfca17a81cbc786d7605694
Francisco Huerta <frhuerta.dwfsc16@ucamp.utel.edu.mx>
2023-03-26 23:16:56 -0600

    tercer cambio

--- js/localstorage.js
+++ js/localstorage.js
@@ -2,7 +2,7 @@ const nombre = document.getElementById("NombreInput");
 const apellido = document.getElementById("ApellidoInput");
 const correo = document.getElementById("correoelectronicoInput");
 const telefono = document.getElementById("NumeroTelefonicoinput");
-const usuarios = document.getElementById("usuarios") || [];
+const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 
 const agregarUsuario = () => {
   const usuario = {
@@ -12,5 +12,34 @@ const agregarUsuario = () => {
     correo: correo.value,
     telefono: telefono.value,
   };
-  console.log(usuario);
+
+  usuarios.push(usuario);
+
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  mostrarUsuarios();
+};
+
+const mostrarUsuario = () => {
+  cuerpoTabla.innerHTML = "";
+  usuarios.forEach((usuario) => {
+    cuerpoTabla.innerHTML += `<tr>
+     <th scope="row">${usuario.id}</th>
+     <td>${usuario.nombre}</td>
+     <td>${usuario.apellido}</td>
+     <td>${usuario.correo}</td>
+     <td>${usuario.telefono}</td>
+     <td>
+     <button
+        type="button"
+        class= "btn btn-danger"
+        onclick="eliminarUsuario('${usuario.id}')"
+        >Eliminar
+        </button><td>
+    </tr>`;
+  });
+};
+
+const eliminarUsuario = (id) => {
+  console.log(id);
+  mostrarUsuarios();
 };
